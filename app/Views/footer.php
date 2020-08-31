ea59798b463e2885dec891eb2f92c77e8e6477fb
pvictorf <41065334+pvictorf@users.noreply.github.com>
2020-08-31 13:40:48 -0300

    Update footer.php

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -1,3 +1,3 @@
 <footer style="background-color: rgba(0, 0, 0, 0.5); height: 80px; position:absolute; bottom:0; left:0; width: 100%;">
-  <p> Copyright © 2020 <?=$name;?>. All Rights Reserved  </p>
+  <p> Copyright © 2020 <?=$name;?>.  </p>
 </footer>
