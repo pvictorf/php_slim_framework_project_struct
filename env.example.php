a7e48894300ae6b2434928ea14e3528589b67cfd
pvictorf <41065334+pvictorf@users.noreply.github.com>
2020-08-31 13:09:32 -0300

    Update env.example.php

--- env.example.php
+++ env.example.php
@@ -1,4 +1,7 @@
 <?php
+//URL
+putenv('PATH_BASE_URL=' . 'https://localhost/public');
+
 //DEBUG
 putenv('DISPLAY_ERROS_DETAILS=' . true);
 
@@ -7,16 +10,15 @@ putenv('DEFAULT_TIMEZONE=' . 'America/Sao_Paulo');
 
 //MYSQL
 putenv('MYSQL_HOST='  . 'localhost');
-putenv('MYSQL_DBNAME='. 'YOUR_DATABASE');
-putenv('MYSQL_USER='  . 'YOUR_USE');
-putenv('MYSQL_PASS='  . 'YOUR_PASS');
+putenv('MYSQL_DBNAME='. '');
+putenv('MYSQL_USER='  . '');
+putenv('MYSQL_PASS='  . '');
 putenv('MYSQL_PORT='  . '3306');
 
 //JWT
-putenv('JWT_SECRET_KEY='  . 'YOUR_PASSWORD_JWT');
+putenv('JWT_SECRET_KEY='  . '');
 
-//PATH VIEWS
+//PATH VIEWS 
 putenv('PATH_VIEWS_TEMPLATE=' . __DIR__ . '/app/Views/');
 putenv('PATH_PARTIALS_TEMPLATE=' . __DIR__ . '/app/Views/partials/');
-putenv('PATH_VIEWS_CACHE=' . __DIR__ . false); 
-putenv('PATH_BASE_URL=' . 'https://localhost/public/');
+putenv('PATH_VIEWS_CACHE=' . __DIR__ . '/app/Views/cache/'); 
