203609d6f06731c9fa372a9f749123bcca4ced89
pvictorf <41065334+pvictorf@users.noreply.github.com>
2020-08-31 17:40:19 -0300

    Update logService.php

--- app/Services/logService.php
+++ app/Services/logService.php
@@ -10,6 +10,8 @@ use Boot\Bootstrap;
     id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     channel VARCHAR(255), 
     level INTEGER, 
+    user_id VARCHAR(40), 
+    origin_id VARCHAR(40), 
     message LONGTEXT, 
     record LONGTEXT,
     time INTEGER UNSIGNED, 
@@ -25,30 +27,42 @@ class LogService {
    /**@var Logger */
    private $logger;
    /**@var array|null */
-   private $context;
+   private $record;
 
-   public function __construct($table = "log", $addtionalFields = ['record'], $context = "web") {
+   public function __construct($table = "log", $addtionalFields = ['user_id', 'origin_id'], $context = "web") {
+
       $pdo = (new Bootstrap)->initializePDO();
-      $this->dbHandler = new MySQLHandler($pdo, $table, $addtionalFields, \Monolog\Logger::DEBUG);
+      $this->dbHandler = new MySQLHandler($pdo, $table, $addtionalFields, \Monolog\Logger::INFO);
      
       $this->logger = new Logger($context);
       $this->logger->pushHandler( $this->dbHandler );
-      $this->logger->pushProcessor(function($record) {
-         $record["extra"]["HTTP_HOST"] = $_SERVER["HTTP_HOST"];
-         $record["extra"]["REQUEST_URI"] = $_SERVER["REQUEST_URI"];
-         $record["extra"]["REQUEST_METHOD"] = $_SERVER["REQUEST_METHOD"];
-         $record["extra"]["HTTP_USER_AGENT"] = $_SERVER["HTTP_USER_AGENT"];
-         $record["extra"]["REMOTE_ADDR"] = $_SERVER["REMOTE_ADDR"];
-
-         return $record;
-      });
-
-      $this->context = $this->logger->getProcessors();
+      $this->record = implode('; ', [
+         "HTTP_HOST" => $_SERVER["HTTP_HOST"],
+         "REQUEST_URI" => $_SERVER["REQUEST_URI"],
+         "REQUEST_METHOD" => $_SERVER["REQUEST_METHOD"],
+         "HTTP_USER_AGENT" => $_SERVER["HTTP_USER_AGENT"],
+         "REMOTE_ADDR" => $_SERVER["REMOTE_ADDR"],
+      ]);
    }
 
-   public function info($menssage = "Log") {
-      
-      $this->logger->info($menssage, ['record' => $this->context]);
+   /** Create a info log in database */
+   public function info($menssage = "", $context = []) {
+      $this->logger->info($menssage, $context);
+   }
+
+   /** Create a warning log in database */
+   public function warning($menssage = "", $context = []) {
+      $this->logger->warning($menssage, $context);
+   }
+
+   /** Create a debug log in database */
+   public function debug($menssage = "", $context = []) {
+      $this->logger->debug($menssage, $context);
+   }
+
+   /** Create a alert log in database */
+   public function alert($menssage = "", $context = []) {
+      $this->logger->alert($menssage, $context);
    }
 }
 
